bc7a68ac114ff2c4bec2dbc49256e448cb876b71
gdoza5 <gdoza5@gmail.com>
2019-11-03 19:02:42 -0600

    found some images

--- js/main.js
+++ js/main.js
@@ -65,3 +65,36 @@ document.addEventListener('keydown', function() {
 
 
 /*----- functions -----*/
+
+
+ function getRandomInt(max) {
+     return Math.floor(Math.random() * Math.floor(max));
+   }
+    let aDInput = getRandomInt(4);
+
+function aDChoice(aDInput){
+       if(aDInput === 3){
+           return document.getElementById('ad_d').textContent = "Bite my shiny metal Axe"
+       }
+      if(aDInput === 1){
+          return document.getElementById('ad_d').textContent = "no compute";
+        
+      }
+      if(aDInput === 0){
+         return document.getElementById('ad_d').textContent = "Yuss"
+       
+      }
+     if(aDInput === 2){
+         return document.getElementById('ad_d').textContent = "JoJo"
+       
+      }
+   }
+
+   console.log(aDInput)
+
+   function play()
+
+   function init() {
+    aDChoice();
+
+   };
